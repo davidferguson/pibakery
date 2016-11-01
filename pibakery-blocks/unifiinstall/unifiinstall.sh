8d4d9711865a64c61ac9c5ad6b0012853da062b1
eddwatts <edd@eddtech.co.uk>
2016-11-01 16:07:11 +0000

    Ubiquiti UniFi Controller install script

--- pibakery-blocks/unifiinstall/unifiinstall.sh
+++ pibakery-blocks/unifiinstall/unifiinstall.sh
@@ -6,5 +6,3 @@ apt-get install unifi oracle-java8-jdk -y
 echo 'ENABLE_MONGODB=no' | sudo tee -a /etc/mongodb.conf > /dev/null
 cp /lib/systemd/system/unifi.service /etc/systemd/system/
 sed -i '/^\[Service\]$/a Environment=JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt' /etc/systemd/system/unifi.service
-#apt-get update && sudo apt-get upgrade -y
-#apt-get install rpi-update && echo Y | sudo rpi-update
